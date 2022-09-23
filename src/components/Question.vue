79b2486caabd7f9a47c0b35c33547bfe8fdf6460
Atefeh Mohammaddoust <atefe.dev@gmail.com>
2022-09-23 20:05:28 +0330

    -a

---
+++ src/components/Question.vue
@@ -0,0 +1,44 @@
+/* eslint-disable vue/multi-word-component-names */
+<template>
+  <div class="questions-ctr">
+    <div class="progress">
+      <div
+        class="bar"
+        :style="{ width: `${(questionAnswered / questions.length) * 100}%` }"
+      ></div>
+      <div class="status">
+        {{ questionAnswered }} out of {{ questions.length }} questions answered
+      </div>
+    </div>
+    <div
+      class="single-question"
+      v-for="(question, qi) in questions"
+      :key="question.q"
+      v-show="questionAnswered === qi"
+    >
+      <div class="question">{{ question.q }}</div>
+      <div
+        class="answers"
+        v-for="answer in question.answers"
+        :key="answer.text"
+        @click.prevent="selectAnswer(answer.is_correct)"
+      >
+        <div class="answer">{{ answer.text }}</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  // eslint-disable-next-line prettier/prettier
+  // eslint-disable-next-line vue/multi-word-component-names
+  props: ["questions", "questionAnswered"],
+  emits: ["question-answered"],
+  methods: {
+    selectAnswer(is_correct) {
+      this.$emit("question-answered", is_correct);
+    },
+  },
+};
+</script>
